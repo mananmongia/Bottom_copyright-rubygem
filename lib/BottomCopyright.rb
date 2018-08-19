5d7b3c8005ab5d913bb9e7221815b7e07f94e01a
Manan Mongia <mongiam4@gmail.com>
2018-08-19 22:01:20 +0530

    base commit

---
+++ lib/BottomCopyright.rb
@@ -0,0 +1,6 @@
+require "BottomCopyright/version"
+require "BottomCopyright/renderer"
+
+module BottomCopyright
+  # Your code goes here...
+end
---
+++ lib/BottomCopyright/version.rb
@@ -0,0 +1,3 @@
+module BottomCopyright
+  VERSION = "0.1.0"
+end
